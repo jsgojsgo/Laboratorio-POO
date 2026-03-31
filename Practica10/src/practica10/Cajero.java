06b1669e4a9a3ce2fc9caf9997d3d96b6b4a35f3
jsgo <jsgo2006@gmail.com>
2026-03-31 11:40:30 -0600

    Cajero y Cliente terminado y actualizado

--- Practica10/src/practica10/Cajero.java
+++ Practica10/src/practica10/Cajero.java
@@ -4,24 +4,36 @@ public class Cajero extends Thread {
 
     private int id;
     private QueueClientes cola;
+    private static final int NUM_CAJEROS = 3;
+    private static String[] estados = new String[NUM_CAJEROS];
 
     public Cajero(int id, QueueClientes cola) {
         this.id = id;
         this.cola = cola;
+        estados[id - 1] = "Libre";
     }
 
     @Override
     public void run() {
-        while(Practica10.ejecutando) {
+        while (Practica10.ejecutando) {
             Cliente cliente = cola.tomarCliente();
             if (cliente == null) continue;
-            System.out.println("[Cajero " + id + "] Cliente " + cliente.getId());
-            try {
-                Thread.sleep(cliente.getTiempoAtencion());
-            } catch (InterruptedException e) {
-                break;
+            for (int i = cliente.getTiempoAtencion() / 1000; i > 0; i--) {
+                estados[id - 1] = "Cliente " + cliente.getId() + " (" + i + "s restantes)";
+                cola.imprimirEstado();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) { break; }
             }
-            System.out.println(">>> Cliente " + cliente.getId() + " terminó\n");
+            estados[id - 1] = "Libre";
+            cola.agregarEvento("Cliente " + cliente.getId() + " terminó en Cajero " + id);
+            cola.imprimirEstado();
+        }
+    }
+
+    public static void imprimirCajeros() {
+        for (int i = 0; i < estados.length; i++) {
+            System.out.println("[Cajero " + (i + 1) + "] " + estados[i]);
         }
     }
 }
--- Practica10/src/practica10/Cliente.java
+++ Practica10/src/practica10/Cliente.java
@@ -5,20 +5,7 @@ import java.util.Random;
 public class Cliente implements Runnable {
 
     private static int contadorGlobal = 1;
-    private int id,  tiempoAtencion;
-    
-    public Cliente(int id, int tiempoAtencion) {
-        this.id = id;
-        this.tiempoAtencion = tiempoAtencion;
-    }
-    public int getId() {
-        return id;
-    }
-    public int getTiempoAtencion() {
-        return tiempoAtencion;
-    }
-
-    //para crear clientes
+    private int id, tiempoAtencion;
     private QueueClientes cola;
     private Random random = new Random();
 
@@ -26,17 +13,24 @@ public class Cliente implements Runnable {
         this.cola = cola;
     }
 
+    public Cliente(int id, int tiempoAtencion) {
+        this.id = id;
+        this.tiempoAtencion = tiempoAtencion;
+    }
+
+    public int getId() { return id; }
+    public int getTiempoAtencion() { return tiempoAtencion; }
+
     @Override
     public void run() {
-        while (true) {
-            int tiempo = (random.nextInt(5) + 1) * 2000;
-            Cliente nuevoCliente = new Cliente(contadorGlobal++, tiempo);
-            cola.agregarCliente(nuevoCliente);
+        while (Practica10.ejecutando) {
+            int tiempo = (random.nextInt(5) + 1) * 1500;
+            Cliente nuevo = new Cliente(contadorGlobal++, tiempo);
+            cola.agregarCliente(nuevo);
+
             try {
-                Thread.sleep(random.nextInt(2000));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+                Thread.sleep(random.nextInt(1500) + 500); 
+            } catch (InterruptedException e) { break; }
         }
     }
 }
