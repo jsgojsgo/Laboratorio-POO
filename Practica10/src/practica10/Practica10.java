426e14dbebc088375329874ee9e0a64e67aa8504
jsgo <jsgo2006@gmail.com>
2026-03-31 11:30:19 -0600

    Practica10 Main actualizado y terminado

--- Practica10/src/practica10/Practica10.java
+++ Practica10/src/practica10/Practica10.java
@@ -1,36 +1,56 @@
 package practica10;
 
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.Scanner;
 
 public class Practica10 {
-
     public static volatile boolean ejecutando = true;
-    
     public static void main(String[] args) {
-
         QueueClientes cola = new QueueClientes();
-
         Cajero c1 = new Cajero(1, cola);
         Cajero c2 = new Cajero(2, cola);
         Cajero c3 = new Cajero(3, cola);
+        c1.setDaemon(true);
+        c2.setDaemon(true);
+        c3.setDaemon(true);
 
         c1.start();
         c2.start();
         c3.start();
-
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-        executor.execute(new Cliente(cola));
-        executor.execute(new Cliente(cola));
         
-        new Thread(() -> {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        Runnable prod1 = new Cliente(cola);
+        Runnable prod2 = new Cliente(cola);
+        Thread t1 = new Thread(prod1);
+        Thread t2 = new Thread(prod2);
+
+        t1.setDaemon(true);
+        t2.setDaemon(true);
+        t1.start();
+        t2.start();
+
+        Thread stopThread = new Thread(() -> {
             Scanner sc = new Scanner(System.in);
             System.out.println("\nPresiona ENTER para detener la simulación...\n");
             sc.nextLine();
-            ejecutando = false;
-            executor.shutdownNow();
-            System.out.println("\nSimulación detenida.");
-        }).start();
+            Practica10.ejecutando = false;
+            synchronized (cola) {
+                cola.notifyAll();
+            }
+            System.out.println("Simulación detenida.");
+        });
+
+        stopThread.setDaemon(true);
+        stopThread.start();
+        try {
+            while (ejecutando) {
+                Thread.sleep(200);
+            }
+        } catch (InterruptedException e) {
+            //terminaprograma
+        }
+
+        System.out.println("Programa finalizado completamente.");
     }
 }
