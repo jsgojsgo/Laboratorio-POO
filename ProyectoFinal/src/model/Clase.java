ff618e1eeaebf7f1f97f98e613f853ec246dad49
jsgo <jsgo2006@gmail.com>
2026-04-02 11:04:59 -0600

    Entrega MODELO Clase.java

---
+++ ProyectoFinal/src/model/Clase.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.io.Serializable;
+
+public class Clase implements Serializable {
+
+    private String nombre, instructor, dia, horaInicio, horaFin;
+    private int cupos;
+
+    public Clase(String nombre, String instructor, String dia, String horaInicio, String horaFin, int cupos) {
+        this.nombre = nombre;
+        this.instructor = instructor;
+        this.dia = dia;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+        this.cupos = cupos;
+    }
+
+    public boolean hayCupo() {
+        return cupos > 0;
+    }
+    public void reducirCupo() {
+        if (cupos > 0) cupos--;
+    }
+    public void aumentarCupo() {
+        cupos++;
+    }
+    public String getNombre() { return nombre; }
+    public String getInstructor() { return instructor; }
+    public String getDia() { return dia; }
+    public String getHoraInicio() { return horaInicio; }
+    public String getHoraFin() { return horaFin; }
+    public int getCupos() { return cupos; }
+}
