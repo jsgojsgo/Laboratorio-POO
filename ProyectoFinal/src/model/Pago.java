a05e215e5ca7070bb3fef5b12e385b511841de10
jsgo <jsgo2006@gmail.com>
2026-04-02 10:58:21 -0600

    Entrega MODELO Pago.java

---
+++ ProyectoFinal/src/model/Pago.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Pago implements Serializable {
+
+    private String titular, numeroTarjeta, fechaExpiracion, cvv;
+    private LocalDate fechaRegistro;
+
+    public Pago(String titular, String numeroTarjeta, String fechaExpiracion, String cvv) {
+        this.titular = titular;
+        this.numeroTarjeta = numeroTarjeta;
+        this.fechaExpiracion = fechaExpiracion;
+        this.cvv = cvv;
+        this.fechaRegistro = LocalDate.now();
+    }
+
+    public String getTitular() { return titular; }
+    public String getNumeroTarjeta() { return numeroTarjeta; }
+    public String getFechaExpiracion() { return fechaExpiracion; }
+    public String getCvv() { return cvv; }
+}
